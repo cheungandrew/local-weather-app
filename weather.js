720f5e49e1f4d1afce803686c1e8969f5f61d3c1
Andrew Cheung <cheungandrew5@gmail.com>
2017-10-29 02:29:27 -0500

    Added local weather app

---
+++ weather.js
@@ -0,0 +1,32 @@
+function showCurrentWeather() {
+
+    // Checking if browser supports Geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success);
+    } else {
+        alert("Browser does not support geolocation.");
+    }
+}
+
+function success(position) {
+    // Obtaining current lat and lon
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+
+    // Creating the API url to get JSON data
+    var api = "https://api.openweathermap.org/data/2.5/";
+    var weather = "weather?"
+    var apiKey = "&appid=782baf5721afe320477b796dedba34f7";
+    var units = "&units=metric";
+    var urlWeather = api + weather + "lat=" + lat + "&lon=" + long + apiKey + units;
+
+
+    $.getJSON(urlWeather, function (data) {
+        $(".city").html("<p>" + data.name + "</p>");
+        $(".temp").html("<p>" + data.main.temp + " &deg;C</p>");
+        $(".icon").html("<img src='https://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
+        $(".desc").html("<p>" + data.weather[0].description);
+    });
+}
+
+showCurrentWeather();
