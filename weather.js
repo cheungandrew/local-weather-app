e61ee57a4dabfb3948bf49b8bf0de692edd50aab
Andrew Cheung <cheungandrew5@gmail.com>
2017-10-29 20:34:08 -0500

    Added Celcius and Fahrenheit converter

--- weather.js
+++ weather.js
@@ -21,12 +21,23 @@ function success(position) {
     var urlWeather = api + weather + "lat=" + lat + "&lon=" + long + apiKey + units;
 
     // Show weather data from JSON
-    $.getJSON(urlWeather, function (data) {
+    $.getJSON(urlWeather, function(data) {
         $(".city").html("<p>" + data.name + "</p>");
         $(".temp").html("<p>" + data.main.temp + " &deg;C</p>");
         $(".icon").html("<img src='https://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
         $(".desc").html("<p>" + data.weather[0].description);
+
+        $(".btn").click(function degreesChange() {
+            if ($(".btn").text() == "Fahrenheit") {
+                $(".btn").html("Celcius");
+                $(".temp").html("<p>" + (data.main.temp * 1.8 + 32) + " &deg;F</p>");
+            } else {
+                $(".btn").html("Fahrenheit");
+                $(".temp").html("<p>" + data.main.temp + " &deg;C</p>");
+            }
+        });
     });
 }
+
 
 showCurrentWeather();
